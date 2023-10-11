9549a74cb6560f079e70e7e746fa8fde2b2adfe4
aluque1 <32274359+aluque1@users.noreply.github.com>
2023-10-11 17:38:51 +0200

    practica 2 cont

---
+++ p2/implementation/xflow_script.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+@REM ###########################################
+@REM # Script file to run the flow 
+@REM # 
+@REM ###########################################
+@REM #
+@REM # Command line for ngdbuild
+@REM #
+ngdbuild -p xc3s1000ft256-5 -nt timestamp -bm practica2.bmm "U:/hlocal/temp/SE/p2/implementation/practica2.ngc" -uc practica2.ucf practica2.ngd 
+
+@REM #
+@REM # Command line for map
+@REM #
+map -o practica2_map.ncd -pr b -ol high -timing -detail practica2.ngd practica2.pcf 
+
+@REM #
+@REM # Command line for par
+@REM #
+par -w -ol high practica2_map.ncd practica2.ncd practica2.pcf 
+
+@REM #
+@REM # Command line for post_par_trce
+@REM #
+trce -e 3 -xml practica2.twx practica2.ncd practica2.pcf 
+
