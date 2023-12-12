2da0c677afd82cc9f1c247f8bd81623d01d15b5d
Aluque <32274359+aluque1@users.noreply.github.com>
2023-12-12 21:22:06 +0100

    funciona salvo lo del reg3 xD

---
+++ practica03/SDK/peripheral_practica03_prog/Debug/src/peripheral_practica03_prog.d
@@ -0,0 +1,31 @@
+src/peripheral_practica03_prog.d: ../src/peripheral_practica03_prog.c \
+ ../src/peripheral_practica03_prog.h \
+ ../../peripheral_practica03_prog_bsp_0/microblaze_0/include/xbasic_types.h \
+ ../../peripheral_practica03_prog_bsp_0/microblaze_0/include/xstatus.h \
+ ../../peripheral_practica03_prog_bsp_0/microblaze_0/include/xbasic_types.h \
+ ../../peripheral_practica03_prog_bsp_0/microblaze_0/include/xio.h \
+ ../../peripheral_practica03_prog_bsp_0/microblaze_0/include/xparameters.h \
+ ../../peripheral_practica03_prog_bsp_0/microblaze_0/include/xil_cache.h \
+ ../../peripheral_practica03_prog_bsp_0/microblaze_0/include/mb_interface.h \
+ ../../peripheral_practica03_prog_bsp_0/microblaze_0/include/xil_types.h \
+ ../../peripheral_practica03_prog_bsp_0/microblaze_0/include/xparameters.h
+
+../src/peripheral_practica03_prog.h:
+
+../../peripheral_practica03_prog_bsp_0/microblaze_0/include/xbasic_types.h:
+
+../../peripheral_practica03_prog_bsp_0/microblaze_0/include/xstatus.h:
+
+../../peripheral_practica03_prog_bsp_0/microblaze_0/include/xbasic_types.h:
+
+../../peripheral_practica03_prog_bsp_0/microblaze_0/include/xio.h:
+
+../../peripheral_practica03_prog_bsp_0/microblaze_0/include/xparameters.h:
+
+../../peripheral_practica03_prog_bsp_0/microblaze_0/include/xil_cache.h:
+
+../../peripheral_practica03_prog_bsp_0/microblaze_0/include/mb_interface.h:
+
+../../peripheral_practica03_prog_bsp_0/microblaze_0/include/xil_types.h:
+
+../../peripheral_practica03_prog_bsp_0/microblaze_0/include/xparameters.h:
